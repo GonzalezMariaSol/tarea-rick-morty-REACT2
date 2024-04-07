20989b465f1216f78f775fd0c9389ede891e652b
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-04-07 02:14:17 -0300

    se rompe cuando busco x nombre - cuando hay filtro la paginacion no funciona - los filtros no se acumulan - el resto ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,84 @@
-import './App.css'
-import { useState, useEffect } from 'react'
-import axios from 'axios';
-import Header from './components/Header'
-import ContainCard from './components/ContainCard'
-import Footer from './components/Footer'
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import ContainCard from "./components/ContainCard";
+import Footer from "./components/Footer";
 
 const App = () => {
+  const [characters, setCharacters] = useState([]); //guarda un array de obj con los primeros 20 personajes y toda su info
+  const [totalPages, setTotalPages] = useState([]); //guarda un arr de obj q siempre es el mismo sobre cuantas hojas totales tiene la api
+  const [pagination, setPagination] = useState(); //guarda solo un numero para luego reutilizarlo de quererlo
+  const [filteredCharacters, setFilteredCharacters] = useState(); //guarda solo el valor del filtro que se eligio
 
-  const [characters, setCharacters] = useState([]) //guarda un array de obj con los primeros 20 personajes y toda su info 
-  const [totalPages, setTotalPages] = useState([]) //guarda un arr de obj q siempre es el mismo sobre cuantas hojas totales tiene la api 
-  const [pagination, setPagination] = useState() //guarda solo un numero para luego reutilizarlo de quererlo
-  const [filteredCharacters, setFilteredCharacters] = useState() //guarda solo el valor del filtro que se eligio
-
-  const [searchedCharacters, setSearchedCharacters] = useState([]) //recibe el valor del input para luego mostrar los resultados que coinciden
-
+  const [searchedCharacters, setSearchedCharacters] = useState([]); //recibe el valor del input para luego mostrar los resultados que coinciden
 
   useEffect(() => {
     let url = "https://rickandmortyapi.com/api/character/";
-    const fetchData = async () => {
-  
-      if (filteredCharacters) {//FUNCIONA
-        url = `https://rickandmortyapi.com/api/character/?status=${filteredCharacters}`
-      } else if (pagination) { 
-        url = `https://rickandmortyapi.com/api/character/?page=${pagination}`
-      } else if (searchedCharacters) {
-        url = `https://rickandmortyapi.com/api/character/?name=${searchedCharacters}`
-      }
 
+    const fetchData = async () => {
+      if (filteredCharacters || pagination || searchedCharacters) {
+        //si se ha seleccionado algo
+        if (filteredCharacters) {
+          //si lo que se selecciono es algun filtro
+          url += `?status=${filteredCharacters}`; //mostrame solos los que cumplan con el filtro
+          console.log(url);
+        }
+        if (pagination) {
+          //si lo que se selecciono fue la pagina
+          url += `?page=${pagination}`; //mostrame la pagina correspondiente
+          console.log(url);
+        }
+
+
+//!!!!!!!!!!!!!!!!!!!!!!! ACA ME QUEDE PENDIENTE
+        if (searchedCharacters) {
+          console.log(characters)
+          const filteredCharacters = characters.filter((character) =>
+            character.name
+              .toLowerCase()
+              .includes(searchedCharacters.toLowerCase())
+          );
+          console.log(filteredCharacters);//me trae solo si coincide algunos de los PRIMEROS 20 
+        }
+        // if (searchedCharacters) {
+        //   url += `?name=${searchedCharacters}`
+        //   console.log(url);
+        // }
+      }
 
       try {
         const { data } = await axios.get(url);
+        console.log(data.results);//solo trae los primeros 20
         setCharacters(data.results);
         setTotalPages(data.info);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-  
+
     fetchData();
   }, [pagination, filteredCharacters, searchedCharacters]);
 
-
   return (
     <>
-    <Header setSearchedCharacters={setSearchedCharacters} characters={characters} setFilteredCharacters={setFilteredCharacters}/>
-    <ContainCard characters={characters} totalPages={totalPages} setPagination={setPagination}/>
-    <Footer />
+      <Header
+        setSearchedCharacters={setSearchedCharacters}
+        characters={characters}
+        setFilteredCharacters={setFilteredCharacters}
+      />
+      <ContainCard
+        characters={characters}
+        totalPages={totalPages}
+        setPagination={setPagination}
+      />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-//*ASK PORQUE SE ME ROMPE CUANDO HAGO LA BUSQUEDA X NOMBRE (EN ALGUN MOMENTO FUNCIONO PERO HACIENDO QUE SE RENDERISEN LOS FILTROS SE HIZO LA ROMPIZION) // PORQUE CUANDO SE APLICA EL FILTRO, MUESTRA UNA PAGINACION CORRECTA PERO SI SE QUIERE USAR NO FUNCIONA?
+//*ASK PORQUE SE ME ROMPE CUANDO HAGO LA BUSQUEDA X NOMBRE (EN ALGUN MOMENTO FUNCIONO PERO HACIENDO QUE SE RENDERISEN LOS FILTROS SE HIZO LA ROMPIZION) 
+//* PORQUE CUANDO SE APLICA EL FILTRO, MUESTRA UNA PAGINACION CORRECTA PERO SI SE QUIERE USAR NO FUNCIONA?
+//*PORQUE NO SE ME ESTAN ACUMULANDO LOS FILTROS?
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { InputBase, IconButton } from '@mui/material';
 import { Search as SearchIcon } from '@mui/icons-material';
-import { useState } from "react";
+// import { useState } from "react";
 
 const NavBar = ({setSearchedCharacters, setFilteredCharacters, characters }) => {
 
-  const [searchValue, setSearchValue] = useState("");
+  // const [searchValue, setSearchValue] = useState("");
 
   const handleStatus = (status) => {
 
@@ -15,15 +15,8 @@ const NavBar = ({setSearchedCharacters, setFilteredCharacters, characters }) => 
 
   const handleSearch = (e) => {
     const value = e.target.value;
-    setSearchValue(value); //GUARDO MI VALOR BUSCADO
-
-    const searchedCharacters = characters.filter(
-      console.log(characters)
-    );
-    // (character) =>
-    //   character.name.toLowerCase().includes(value.toLowerCase())
-    console.log(searchedCharacters);//devuelve un arr de objetos de todos los personajes que contienen esa letra ingresada
-    // setSearchedCharacters(filteredCharacters);
+    setSearchedCharacters(value); //GUARDO MI VALOR BUSCADO
+    console.log(value);
     }
 
   return (
